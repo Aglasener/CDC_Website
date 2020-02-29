0b01ead018b75a45b583380eb6f43456862a7020
Lisa Walter <epw1389@gmail.com>
2020-02-29 14:15:42 -0500

    risk table and data in csv

--- models/schema.sql
+++ models/schema.sql
@@ -3,3 +3,28 @@ CREATE DATABASE CDCdb;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+
+DROP DATABASE IF EXISTS riskvalues_db;
+CREATE database riskvalues_db;
+
+USE riskvalues_db;
+
+CREATE TABLE riskvalues (
+  ID INT NOT NULL,
+  factor VARCHAR(100) NOT NULL,
+  options VARCHAR(100) NULL,
+  points INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE riskresults (
+  ID INT NOT NULL,
+  points INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+
+
+
+
