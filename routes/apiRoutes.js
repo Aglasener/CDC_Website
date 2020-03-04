380bfc6064d25d123e70c3c1cb63bd15e1cd1c89
Elijah Broaddus <elijahlb@gmail.com>
2020-03-04 16:52:33 -0500

    basic api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,54 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // RESULT TABLE
+  // Get all results
+  app.get("/api/results", function(req, res) {
+    db.Result.findAll({}).then(function(dbResult) {
+      res.json(dbResult);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Create a new user result
+  app.post("/api/results", function(req, res) {
+    db.Result.create(req.body).then(function(dbResult) {
+      res.json(dbResult);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  // Delete an  by id
+  app.delete("/api/results/:id", function(req, res) {
+    db.Result.destroy({ 
+      where: { id: req.params.id 
+      } 
+    }).then(function(dbResult) {
+      res.json(dbResult);
+    });
+  });
+
+//-----------------------------------------------------
+
+  // USER TABLE
+  app.get("/api/user", function(req, res) {
+    db.User.findAll({}).then(function(dbUser) {
+      res.json(dbUser);
+    });
+  });
+
+  // Create a new user result
+  app.post("/api/user", function(req, res) {
+    db.User.create(req.body).then(function(dbUser) {
+      res.json(dbUser);
+    });
+  });
+
+  // Delete an  by id
+  app.delete("/api/user/:id", function(req, res) {
+    db.User.destroy({ 
+      where: { id: req.params.id 
+      } 
+    }).then(function(dbUser) {
+      res.json(dbUser);
     });
   });
 };
