7a2e6fef918f6a594af698528229edfef273363c
EarlBlue <blue.earl.blue@gmail.com>
2020-03-05 22:18:29 -0500

    Added config and controller templates, updated routing for testing

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,22 +2,20 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+  app.get('/', function (req, res) {
+    res.render('prompt');
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/list", function(req, res) {
+    res.render('list');
+  });
+
+  app.get('/user', function (req, res) {
+    res.render('user');
+  });
+
+  app.get("/form", function(req, res) {
+    res.render('form');
   });
 
   // Render 404 page for any unmatched routes
@@ -25,3 +23,31 @@ module.exports = function(app) {
     res.render("404");
   });
 };
+
+// var db = require("../models");
+
+// module.exports = function(app) {
+//   // Load index page
+//   app.get("/", function(req, res) {
+//     db.Example.findAll({}).then(function(dbExamples) {
+//       res.render("index", {
+//         msg: "Welcome!",
+//         examples: dbExamples
+//       });
+//     });
+//   });
+
+//   // Load example page and pass in an example by id
+//   app.get("/example/:id", function(req, res) {
+//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.render("example", {
+//         example: dbExample
+//       });
+//     });
+//   });
+
+//   // Render 404 page for any unmatched routes
+//   app.get("*", function(req, res) {
+//     res.render("404");
+//   });
+// };
